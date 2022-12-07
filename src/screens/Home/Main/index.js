ad78dd128c3d6a2abecffee7caab6ea185aad341
Joshua Mathew <jm@Joshuas-iMac.local>
2022-12-07 17:46:10 +0530

    hero img edit

--- src/screens/Home/Main/index.js
+++ src/screens/Home/Main/index.js
@@ -7,38 +7,39 @@ import Cards from "./Cards";
 
 const Main = ({ scrollToRef }) => {
     return (
-        <div className={cn("section", styles.main)}>
-            <div className={cn("container", styles.container)}>
-                <div className={styles.wrap}>
-                    <h1 className={cn("h1", styles.title)}>
-                     Web3 Infrastructure<br></br>for Digital Experiences
-                    </h1>
-                    <div className={styles.text}>
-                    <b>Tesseract</b> is the leading decentralised infrastructure for builders and creators in Web3.
-                    </div>
-                    <Link className={cn("button", styles.button)} to="/sign-up">
-                        Start now
-                    </Link>
-                    <ScrollButton
-                        onScroll={() =>
-                            scrollToRef.current.scrollIntoView({
-                                behavior: "smooth",
-                            })
-                        }
-                        className={styles.scroll}
-                    />
-                </div>
-                <div className={styles.bg}>
-                    <img
-                        srcSet="/images/content/9.png 2x"
-                        src="/images/content/9.png"
-                        alt="Cards"
-                    />
-                </div>
-                <Cards className={styles.cards} />
-            </div>
-        </div>
-    );
+			<div className={cn('section', styles.main)}>
+				<div className={cn('container', styles.container)}>
+					<div className={styles.wrap}>
+						<h1 className={cn('h1', styles.title)}>
+							Web3 Infrastructure<br></br>for Digital Experiences
+						</h1>
+						<div className={styles.text}>
+							<b>Tesseract</b> is the leading decentralised infrastructure for
+							builders and creators in Web3.
+						</div>
+						<Link className={cn('button', styles.button)} to='/sign-up'>
+							Start now
+						</Link>
+						<ScrollButton
+							onScroll={() =>
+								scrollToRef.current.scrollIntoView({
+									behavior: 'smooth',
+								})
+							}
+							className={styles.scroll}
+						/>
+					</div>
+					<div className={styles.bg}>
+						<img
+							srcSet='/images/content/eth-server-dark@2x.png 2x'
+							src='/images/content/eth-server-dark.png'
+							alt='Cards'
+						/>
+					</div>
+					<Cards className={styles.cards} />
+				</div>
+			</div>
+		)
 };
 
 export default Main;
